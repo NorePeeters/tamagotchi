85ad71ff659c656dbb9f878b4650d3ea4114af49
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-04-18 14:56:44 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    voeding += 5
+})
+input.onButtonPressed(Button.B, function () {
+    knuffel += 5
+})
+let dood = 0
+let knuffel = 0
+let voeding = 0
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    basic.pause(1000)
+    voeding += -1
+})
+basic.forever(function () {
+    if (knuffel < 10 || voeding < 10) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+basic.forever(function () {
+    basic.pause(1000)
+    knuffel += -1
+})
+basic.forever(function () {
+    if (knuffel > 10 && voeding > 10) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+basic.forever(function () {
+    if (knuffel < 0 || voeding < 0) {
+        dood += 1
+    }
+})
+basic.forever(function () {
+    if (dood == 1) {
+        basic.showIcon(IconNames.Asleep)
+    }
+})
