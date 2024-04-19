bab671e1b0837c4d004e72893ae4ea23c51624fa
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-04-19 13:20:15 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -11,17 +11,15 @@ basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     basic.pause(1000)
     voeding += -1
-})
-basic.forever(function () {
     basic.pause(1000)
     knuffel += -1
 })
 basic.forever(function () {
-    if (knuffel < 10 || voeding < 10) {
-        basic.showIcon(IconNames.Sad)
-    }
     if (knuffel > 10 && voeding > 10) {
         basic.showIcon(IconNames.Happy)
+    }
+    if (knuffel < 10 && knuffel > 0 || voeding < 10 && voeding > 0) {
+        basic.showIcon(IconNames.Sad)
     }
     if (knuffel < 0 || voeding < 0) {
         dood += 1
