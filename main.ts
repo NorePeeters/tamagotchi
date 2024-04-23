3c2cc99be32e118c67a235c52966d61835abfe1a
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-04-23 11:15:49 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    voeding += 5
+    if (dood == 0) {
+        voeding += 5
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    knuffel += 5
+    if (dood == 0) {
+        knuffel += 5
+    }
 })
 let dood = 0
-let knuffel = 0
-let voeding = 0
-basic.showIcon(IconNames.Happy)
+dood = 0
+let knuffel = 20
+let voeding = 20
 basic.forever(function () {
     basic.pause(1000)
     voeding += -1
-    basic.pause(1000)
     knuffel += -1
 })
 basic.forever(function () {
@@ -23,8 +26,6 @@ basic.forever(function () {
     }
     if (knuffel < 0 || voeding < 0) {
         dood += 1
-    }
-    if (dood == 1) {
         basic.showIcon(IconNames.Asleep)
     }
 })
